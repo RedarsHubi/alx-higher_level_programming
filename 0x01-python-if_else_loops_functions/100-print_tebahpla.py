58b28c8ba64ec4c2fda56b38688016b12dff3f62
RedarsHubi <RedarsHubi@github.com>
2023-10-03 10:01:31 -0700

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 result = ""
 for i in range(123, 96, -1):
-    if i%2 == 0:
+    if i % 2 == 0:
         result += chr(i)
     else:
-        result += (chr((i - 32)))
+        result += chr((i - 32))
 print("{}".format(result), end="")    
