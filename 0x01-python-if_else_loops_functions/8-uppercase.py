c340b576ddabf41935409df1858a9a9a9b82116a
RedarsHubi <RedarsHubi@github.com>
2023-10-03 09:09:46 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,15 @@
 
 
 def uppercase(str):
+    result = ""
     index = 0
     for i in str:
         if ord(i) >= 97 and ord(i) < 123:
-            print("{}".format(chr(ord(i)-32)),
-                  end="" if index < len(str) - 1 else"\n")
+            result += (chr(ord(i)-32)) if index < len(str) - 1 else "\n"
         elif ord(i) >= 65 and ord(i) < 91:
-            print("{}".format(i), end="")
+            result += i
+        elif i == " " or i == ",":
+            resutl += i
+
         index = index + 1
+    print(result)
