5e162c641d236603982d1c3d64e0fca26eee0ea4
RedarsHubi <RedarsHubi@github.com>
2023-10-03 10:40:25 -0700

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     copy = ""
-    for i in range str:
-        if i == n:
-            copy += ""
-        else:
-            copy += i
+    for i in range (len(str)):
+        if i != n:
+            copy += str[i]
     return copy
