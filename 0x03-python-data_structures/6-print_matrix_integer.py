0d01f5a8c4c435a08b6eda34c38cc57be1b11ae9
RedarsHubi <RedarsHubi@github.com>
2023-10-09 09:05:42 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for row in matrix:
+        if len(row) == 0:
+            print()
         for element in row:
-	    if element == row[-1]:
+            if element == row[-1]:
                 print("{:d}".format(element), end='')
             else:
                 print("{:d}".format(element), end=' ')
