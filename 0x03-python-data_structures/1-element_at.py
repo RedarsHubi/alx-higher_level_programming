cbee7ebc5f93108d8d51df36f52d5283b87a5391
RedarsHubi <RedarsHubi@github.com>
2023-10-09 07:02:38 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     index = 0
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return None
     for i in my_list:
         if index == idx:
