e145b83c334b067aa953259be2196e6a503a0309
RedarsHubi <RedarsHubi@github.com>
2023-10-09 08:46:42 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        if i == len(matrix) - 1:
-            print("{:d}".format(matrix[i]), end=',\n')
-        else:
-            print("{:d}".format(matrix[i]))
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=' ')
