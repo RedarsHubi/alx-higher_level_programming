4c205881b86c6af78568fc25da5760cf245ffabb
RedarsHubi <RedarsHubi@github.com>
2023-10-09 07:49:54 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,5 @@ def new_in_list(my_list, idx, element):
         return copy_l
     for i in range(len(my_list)):
         if i == idx:
-            copy_l[i] == element
+            copy_l[i] = element
             return copy_l
