8aca8e11690a3dc5522dc07c02ede83d0acb78fa
RedarsHubi <RedarsHubi@github.com>
2023-10-12 05:34:42 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    new_l = my_list[:]
+    for i in range(len(new_l)):
+        if new_l[i] == search:
+            new_l[i] = replace
+    return new_l
