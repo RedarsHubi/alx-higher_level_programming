be57f0fdfef06acda8849b6a33f56cfefedf0d9b
RedarsHubi <RedarsHubi@github.com>
2023-10-12 07:13:10 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     store = None
-    for val in a_dictionary.values():
+    b_student = None
+    for student, val in a_dictionary.items():
         if val > store:
             store = val
-    return store
+            b_student = student
+    return b_student
