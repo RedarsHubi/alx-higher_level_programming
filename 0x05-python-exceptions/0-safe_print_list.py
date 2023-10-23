948c4acc71efbac37e473e90b01819a473f86adf
RedarsHubi <RedarsHubi@github.com>
2023-10-23 05:46:30 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    i = 0
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-            count += 1
+        while i is not x:
+            print(my_list[i], end='')
+            i += 1
     except range_error:
         pass
     print()
-    return count
+    return i
