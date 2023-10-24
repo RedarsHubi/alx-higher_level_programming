544560787ecacdf8dbeef508a44a0dd598a77777
RedarsHubi <RedarsHubi@github.com>
2023-10-24 12:25:04 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """constructor
+
+        Args:
+            size: size of one side of the square.
+        """
+        self.__size = size
