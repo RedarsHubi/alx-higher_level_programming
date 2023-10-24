dc7adcb0f1d6383957edc9999fefa4846591575f
RedarsHubi <RedarsHubi@github.com>
2023-10-24 12:52:03 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,7 +21,7 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = size
 
-	    def area(self):
+    def area(self):
         """Area square
 
         Returns:
