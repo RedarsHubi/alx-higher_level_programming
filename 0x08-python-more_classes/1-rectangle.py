7a8a501d004b6567d97386862ec9aecdc3f1aac0
RedarsHubi <RedarsHubi@github.com>
2023-10-30 07:09:02 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter for priv inst att width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for priv inst att height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
