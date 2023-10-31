a2ce72f22fc425e1ddae46550f7a3b1d71a0f9ba
RedarsHubi <RedarsHubi@github.com>
2023-10-31 04:46:14 -0700

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-
-Module for class prevents the user from dynamically
-creating new instance attributes, except for 1 case
-
-"""
+"""Module for class prevents the user from dynamically creating instances"""
 
 
 class LockedClass:
     __slots__ = ['first_name']
 
     def __init__(self):
-        """ Init method """
+        """ Initalizing meth """
         pass
