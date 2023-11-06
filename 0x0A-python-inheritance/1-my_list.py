d4dd467d4a3459207dc7b636490983a43286ac5e
RedarsHubi <RedarsHubi@github.com>
2023-11-06 06:25:20 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for available attributes and methods of an object"""
+
+
+class Mylist(list):
+    """prints list in a sorted manner"""
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
