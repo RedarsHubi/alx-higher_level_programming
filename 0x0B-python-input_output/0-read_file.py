9f9bfa270af456f658f5292e9923d0c0f5d5c721
RedarsHubi <RedarsHubi@github.com>
2023-11-07 05:19:28 -0800

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Reads and prints"""
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         data = f.read()
     print(data)
