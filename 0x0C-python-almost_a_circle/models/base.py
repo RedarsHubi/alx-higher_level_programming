62a5b7a7f89c23287f711bb268c44855d337fa1e
root <redarshubi@github.com>
2023-11-13 10:55:20 -0800

    mandatory

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,10 +5,11 @@
 class Base:
     """Representation of base of all future classes"""
     __nb_objects = 0
+
     def __init__(self, id=None):
-    """Class constructor"""
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        """Class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
