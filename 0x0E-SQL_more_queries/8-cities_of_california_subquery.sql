2430cd5a98ae8643636b612bbfbb04429c17d73f
RedarsHubi <RedarsHubi@github.com>
2023-12-13 08:18:48 -0800

    8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- script that lists all the cities of California
+SELECT cities.id, cities.name FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
