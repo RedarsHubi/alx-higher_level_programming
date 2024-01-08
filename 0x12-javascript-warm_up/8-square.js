2c3f8e745065997a0c5d1400c984d00063d8c7b9
root <redarshubi@github.com>
2024-01-08 08:38:15 -0800

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 let i = 0;
 let j = 0;
-let store;
 const rip = parseInt(process.argv[2]);
 if (Number.isNaN(rip)) {
   console.log('Missing size');
 } else {
   for (i = 0; i < rip; i++) {
+    let store = '';
     for (j = 0; j < rip; j++) {
       store += 'X';
     }
