d9f0c827ae184c39742efdad943b6af3ace4d0a4
root <redarshubi@github.com>
2024-01-08 08:28:19 -0800

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let num = parseInt(process.argv[2]);
-if (Number.isNaN(i)) {
+if (Number.isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
   while (num > 0) {
