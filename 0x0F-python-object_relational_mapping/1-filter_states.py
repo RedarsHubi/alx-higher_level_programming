1a64cdd8acca8ecbb0ce8756a129a800453a165d
root <redarshubi@github.com>
2024-01-12 04:31:31 -0800

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Listing states module"""
+
+
+import MySQLdb
+import sys
+
+
+def filter_st(usrnm, psswrd, db_nm):
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=usrnm,
+            passwd=psswrd,
+            db=db_nm,
+            charset="utf8"
+        )
+
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
+        cursor.execute(query)
+
+        states = cursor.fetchall()
+
+        for zab in states:
+            print(zab)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if 'connection' in locals() and connection.open:
+            cursor.close()
+            connection.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        usrnm, psswrd, db_nm = sys.argv[1], sys.argv[2], sys.argv[3]
+
+        filter_st(usrnm, psswrd, db_nm)
