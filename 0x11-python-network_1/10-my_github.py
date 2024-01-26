f8d40da459a25a0b50278aa8d9d93419cfdbf412
root <redarshubi@github.com>
2024-01-26 09:21:52 -0800

    10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that takes GitHub credentials"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = "redarshubi"
+    password = ("github_pat_11A7URVVA0uRJ"
+                "L2MnKiw6L_bMq87RsTLW07aBYOfHjW"
+                "eHSjnUHFJIoaTNY6j3uiYpl4Q6UMCCRRjIw7mAo")
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print("Your GitHub user id is:", user_data.get('id'))
+    else:
+        print("Failed to fetch user data. Status code:", response.status_code)
